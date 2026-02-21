a517bc3a341aac6dc3abeb1a68af8124f3fe5475
Tugra Gulen <tugra.gulen.tedu@gmail.com>
2026-02-21 13:36:36 +0300

    Project initialization.

---
+++ service-finance-tracker/src/main/java/com/tugragulen/finance_tracker/FinanceTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.tugragulen.finance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApplication.class, args);
+        System.out.println("hello world");
+	}
+
+}
