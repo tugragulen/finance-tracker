8b91c533c73c6c75502167b2c8486e85e6ad4e91
Tugra Gulen <tugra.gulen.tedu@gmail.com>
2026-02-21 14:12:02 +0300

    Base entity updated.

--- service-finance-tracker/src/main/java/com/tugragulen/finance_tracker/entity/BaseEntity.java
+++ service-finance-tracker/src/main/java/com/tugragulen/finance_tracker/entity/BaseEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -21,6 +23,9 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 }
